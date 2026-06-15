772f2c2c3e95b13413cbba703d4285d4b6e907f6
Anobil Emmanuel <anobilemmnauel2@gmail.com>
2026-06-15 00:50:49 +0000

    fixed home route and added home page

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,14 @@ function ensureAuth(req, res, next) {
     return res.redirect('/login');
 }
 
+
+router.get('/', (req, res) => {
+    res.render('home');
+});
 router.get('/login', (req, res) => {
     res.render('login');
 });
+
 
 router.get('/signup', (req, res) => {
     res.render('signup');
